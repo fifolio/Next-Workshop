9df9cf1fdadfec06f8174dfddea705126c480fdc
Firas <firasdabbabi@gmail.com>
2023-03-24 17:31:49 +0100

    version 1.1: Create Pages

---
+++ 1-space-project/pages/index.js
@@ -0,0 +1,10 @@
+import AboutPage from "./about";
+
+export default function HomePage(){
+    return (
+        <div>
+            <h1>Hello world</h1>
+            <AboutPage />
+        </div>
+    )
+}
