9b3b8c0b304c5e418af297f630996ec9d3cd3c42
FIRAS <97522852+fifolio@users.noreply.github.com>
2023-03-29 11:38:56 +0100

    Delete 1-space-project directory

--- 1-space-project/pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/global.css'
-
-export default function App(props) {
-    const {Component, pageProps} = props
-    return (
-        <Component {...pageProps} />
-    )
-}
--- 1-space-project/pages/index.js
+++
@@ -1,9 +0,0 @@
-import style from '../styles/Home.module.css'
-
-export default function HomePage(){
-    return (
-        <div className={style.section}>
-            <h1>Discover our Space</h1>
-        </div>
-    )
-}
